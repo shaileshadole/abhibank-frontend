751620b9a1e9ee83563c8802c0cf2859d267f409
Shailesh Adole <adoleshailesh2@gmail.com>
2025-12-18 16:26:26 +0530

    ATM Machine height fixed

--- src/components/ATMMachine.jsx
+++ src/components/ATMMachine.jsx
@@ -1,6 +1,16 @@
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import ChangePin from "../pages/ChangePin";
+import CheckBalance from "../pages/CheckBalance";
+import Deposite from "../pages/Deposite";
+import Withdraw from "../pages/Withdraw";
+import MainMenu from "../pages/MainMenu";
+import Receipt from "../components/Receipts/Receipt";
+import MiniStatement from "../pages/MiniStatement";
+import FullStatement from "../pages/FullStatement";
+
+
 import "./ATMMachine.css";
 import {
   BrowserRouter as Router,
@@ -21,6 +31,14 @@ const ATMMachine = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/users/register" element={<Register />} /> 
                 <Route path="/users/login" element={<Login />} /> 
+                <Route path="/users/change-pin" element={<ChangePin />} />
+                <Route path="/users/check-balance" element={<CheckBalance />} />
+                <Route path="/users/deposit" element={<Deposite />} />
+                <Route path="/users/withdraw" element={<Withdraw />} />
+                <Route path="/users/main-menu" element={<MainMenu />} />
+                <Route path="/transactions/threehistory" element={<MiniStatement />} />
+                <Route path="/transactions/fullhistory" element={<FullStatement />} />
+                <Route path="/receipt" element={<Receipt />} />
               </Routes>
             </BrowserRouter>
           </div>
